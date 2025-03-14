a6a81bf7a13822bae8f95c742316d6d017692822
Shuojiang Liu <shuojiang.liu@nio.io>
2025-03-13 17:46:55 -0700

    Reenable weight sharing config for SA8295

--- tools/qnn_converter/generate_binary.py
+++ tools/qnn_converter/generate_binary.py
@@ -58,7 +58,8 @@ def generate_context_binary():
                 "soc_model": soc_map[args.soc].soc_id,
             },
         ],
-        "context": {"weight_sharing_enabled": False if args.soc == "sa8295" else True},
+        # "context": {"weight_sharing_enabled": False if args.soc == "sa8295" else True},
+        "context": {"weight_sharing_enabled": True},
     }
 
     htp_config = {
